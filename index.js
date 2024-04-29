113d87125902741bd6c5e2b844003dd1933fb443
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-29 18:45:11 -0300

    tipo de resposta

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.listen(3000, () => {
-    console.log('backend funcionando!!');
-})
+
+app.get("/index", (require, response) => {
+  response.json({
+    data: [
+      { id: 7, name: "Ana", position: 1 },
+      { id: 34, name: "Bia", position: 2 },
+      { id: 73, name: "Carlos", position: 3 },
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200,
+  });
+});
+
+app.listen(8080, () => {
+  console.log("backend funcionando!!");
+});
